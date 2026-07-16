97b486f5ebe77aa34a0928cc5ed479417e2cb827
Sona <sonie.zeynalova@gmail.com>
2026-07-16 18:30:56 +0400

    add task

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,45 @@ import { useState } from 'react';
 
 function App() {
 
-  const [tasks, setTasks] = useState([{id: 1, name: 'task1'}, {id: 2, name: 'task2'}, {id: 3, name: 'task3'}]);
+  //const [tasks, setTasks] = useState([{id: 1, name: 'task1'}, {id: 2, name: 'task2'}, {id: 3, name: 'task3'}]);
+  const [tasks, setTasks] = useState(['task1','task2','task3']);
+  const [taskInput, setTaskInput] = useState('');
 
+  function handleTaskInput(event) {
+    setTaskInput(event.target.value);
+  }
   function addTask() {
-    alert('Task added!')
+    setTasks([...tasks, taskInput]);
   }
 
-  function TaskCard(task) {
+  function TaskCard({task}) {
     const [isDone, setIsDone] = useState(false);
 
-    function handleButton() {
+    function handleDone() {
       setIsDone(isDone ? false : true);
+    }
+    function handleEdit() {
+
+    }
+    function handleDelete() {
+
     }
     return(
       <div className='task'>
-        <button className='done-button' onClick={handleButton}>{isDone ? "done" : ""}</button>
-        <p>{task.name}</p>
-
+        <button className='done-button' onClick={handleDone}>{isDone ? "done" : "not done"}</button>
+        {task}
+        <button className='edit-button' onClick={handleEdit}>Edit</button>
+        <button className='delete-button' onClick={handleDelete}>Delete</button>
       </div>
     )
   }
   
   return(
     <div className="container">
-      <input className='input' placeholder='Enter a task'/>
+      <input className='input' placeholder='Enter a task' value={taskInput} onChange={handleTaskInput}/>
       <button className='add-button' onClick={addTask}>Add</button>
       <div className='tasks'>
-        {tasks.map(task => <TaskCard task={task} key={task.id}/>)}
+        {tasks.map(task => <TaskCard task={task} key={task}/>)}
       </div>
     </div>
   )

4ec21a7ab35ad750d0a115a8d83f251271452d7c
Sona <sonie.zeynalova@gmail.com>
2026-07-16 19:29:49 +0400

    save to local storage and delete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TaskCard from './TaskCard.jsx';
 
 function App() {
-  const [tasks, setTasks] = useState(['task1','task2','task3']);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')) || []);
   const [taskInput, setTaskInput] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   function handleTaskInput(event) {
     setTaskInput(event.target.value);
   }
   function addTask() {
-    setTasks([...tasks, taskInput]);
+    if (taskInput) {
+      setTasks(t => [...t, {id: Date.now(), name: taskInput}]);
+      setTaskInput(''); 
+    }
   }
+  function deleteTask(taskId) {
+    setTasks(t => t.filter(task => task.id !== taskId));
+  }
+  
   
   return(
     <div className="container">
+      
       <input className='input' placeholder='Enter a task' value={taskInput} onChange={handleTaskInput}/>
+
       <button className='add-button' onClick={addTask}>Add</button>
+
       <div className='tasks'>
-        {tasks.map(task => <TaskCard task={task} key={task}/>)}
+        {tasks.map(task => <TaskCard task={task} key={task.id} onDelete={deleteTask}/>)}
       </div>
+      
     </div>
   )
 }
--- src/TaskCard.jsx
+++ src/TaskCard.jsx
@@ -1,21 +1,21 @@
 import {useState} from 'react'; 
 
-function TaskCard({task}) {
+function TaskCard({task, onDelete}) {
   const [isDone, setIsDone] = useState(false);
 
   function handleDone() {
-    setIsDone(isDone ? false : true);
+    setIsDone(!isDone);
   }
   function handleEdit() {
 
   }
   function handleDelete() {
-
+    onDelete(task.id);
   }
   return(
     <div className='task'>
       <button className='done-button' onClick={handleDone}>{isDone ? "done" : "not done"}</button>
-      {task}
+      {task.name}
       <button className='edit-button' onClick={handleEdit}>Edit</button>
       <button className='delete-button' onClick={handleDelete}>Delete</button>
     </div>
